20e9b8a4f91dac5bd07c494ddb4033c1260a3337
zai-i <zainab@wonderbly.com>
2023-07-11 19:57:55 +0100

    Instructions

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import './App.scss';
 import { useState } from 'react';
 
 function App() {
-  const [keyword1, setKeyword1] = useState('')
-  const [keyword2, setKeyword2] = useState('')
+  const [keyword, setKeyword] = useState('')
   const [couplet, setCouplet] = useState('')
   const [spinner, setSpinner] = useState(false);  
 
-  const generate = (keyword1, keyword2) => {
-
+  const search = (keyword) => {
     setSpinner(true);
-    fetch(`${import.meta.env.VITE_SITE_URL}/api?keyword1=${keyword1}&keyword2=${keyword2}`)
+    fetch(`${import.meta.env.VITE_SITE_URL}/api?keyword=${keyword}`)
     .then(response => response.text() 
     .then(function (response) {
       setSpinner(false);
@@ -25,24 +23,19 @@ function App() {
 
 const handleSubmit = async (event) => {
   event.preventDefault();
-  setCouplet(generate(keyword1, keyword2))
+  setCouplet(search(keyword))
 }
 
   return (
     <main>
 
-    <p className="instructions">Lyriki looks for rhyming couplets collected from 2 wikipedia articles</p>
+    <p className="instructions">Lyriki creates rhyming couplets from wikipedia articles and famous sonnets!</p>
     <form onSubmit={handleSubmit}>
         <input 
         type="text" 
         name="keyword1" 
-        onChange={event => setKeyword1(event.target.value)}
+        onChange={event => setKeyword(event.target.value)}
         placeholder="keyword here"/>
-        <input 
-        type="text" 
-        name="keyword2" 
-        onChange={event => setKeyword2(event.target.value)}
-        placeholder="another keyword here"/>
       
         <button type="submit">search!</button>
     </form>
