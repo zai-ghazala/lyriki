d442c68985f4e1a71b8d0551fb4380c49c4f5a54
zai-i <zainab@wonderbly.com>
2023-07-11 18:28:39 +0100

    Instructions

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const handleSubmit = async (event) => {
   return (
     <main>
 
-    <p className="instructions">Lyriki looks for rhyming couplets collected from 2 Wikipedia articles</p>
+    <p className="instructions">Lyriki looks for rhyming couplets collected from 2 wikipedia articles</p>
     <form onSubmit={handleSubmit}>
         <input 
         type="text" 
