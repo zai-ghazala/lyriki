bb8bc5855d25f1ac787bad8091598dc1f327f539
zai-i <zainab@wonderbly.com>
2023-07-11 20:33:39 +0100

    Fonts

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import { useState } from 'react';
 
 function App() {
   const [keyword, setKeyword] = useState('')
-  const [couplet, setCouplet] = useState('')
+  const [couplet, setCouplet] = useState()
   const [spinner, setSpinner] = useState(false);  
 
   const search = (keyword) => {
     setSpinner(true);
     fetch(`${import.meta.env.VITE_SITE_URL}/api?keyword=${keyword}`)
-    .then(response => response.text() 
+    .then(response => response.json()
     .then(function (response) {
       setSpinner(false);
       setCouplet(response)
@@ -28,18 +28,24 @@ const handleSubmit = async (event) => {
 
   return (
     <main>
+      <p className="instructions">Lyriki stitches together unlikely rhyming couplets from wikipedia articles and famous sonnets! <br/><br/>Made by <a href="https://www.zaiz.ai">Zai</a>
+        {couplet ?
+          <>
+            <br/><br/>
+            <span className="title">{couplet.title}</span>
+            <span className="author">— {couplet.author}</span>
+            <br/>
+            <a href={`https://en.wikipedia.org/wiki/${keyword}`}>wikipedia article</a></> : null}
+      </p><form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="keyword1"
+          onChange={event => setKeyword(event.target.value)}
+          placeholder="keyword here" />
 
-    <p className="instructions">Lyriki stitches together unlikely rhyming couplets from wikipedia articles and famous sonnets!</p>
-    <form onSubmit={handleSubmit}>
-        <input 
-        type="text" 
-        name="keyword1" 
-        onChange={event => setKeyword(event.target.value)}
-        placeholder="keyword here"/>
-      
         <button type="submit">search!</button>
-    </form>
-    {spinner ? <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> : couplet ? <><div className="couplet">{couplet}</div><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></> : null}
+      </form>
+    {spinner ? <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> : couplet ? <><div className="couplet">{couplet.random}</div><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></> : null}
     </main>
   );
 }
